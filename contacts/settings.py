f2f125b6f05033fa351bc131d477dca9c8bcc8db
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-03 23:03:40 +0600

    Message Alert Disappeared

--- contacts/settings.py
+++ contacts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e&(zxny!!&xr^wjqc0!r8hzkajcfw$yq$jqn78x!)@my63x8%i'
+SECRET_KEY = 'e&(zxny!!&xr^wjqomarc0!r8hzkajcfw$yfarukq$jqn78x!)@my63x8%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # False because, we created our custom error page
